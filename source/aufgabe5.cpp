155417fa7b8e4fa445c9c6576fdf9618733cd62a
annachiu7 <qujiani7879@126.com>
2016-05-16 18:01:27 +0200

    Modify changes for docs up to 3.8

--- source/aufgabe5.cpp
+++ source/aufgabe5.cpp
@@ -5,6 +5,11 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <set>
+#include "circle.hpp"
+#include "vec2.hpp"
+#include "mat2.hpp"
+#include "color.hpp"
 
 int randNum()
 {
@@ -16,6 +21,23 @@ bool is_even(int a)
 	return (a%2)==0;
 }
 
+template<typename T>
+void swap(T & a, T & b) // & no copie, otherwise values dont change
+{
+	T c = a;
+	a = b;
+	b = c;
+}
+
+/*
+void swap(int *a, int *b)
+{
+	int c=*a;
+	*a=*b;
+	*b=t;
+}
+*/
+
 TEST_CASE("describe_factorial", "[aufgabe3]")
 {
 	std::vector<unsigned int> v(100);
@@ -25,6 +47,25 @@ TEST_CASE("describe_factorial", "[aufgabe3]")
 	
 	REQUIRE(std::all_of(v.begin(),v.end(), is_even));
 }
+
+TEST_CASE("describe_sorted_circle", "[aufgabe3.7]")
+{
+	std::set<Circle> s;
+	for (int i = 15; i > 5; --i)
+	{
+		s.insert(Circle{i});
+	}
+	
+	REQUIRE(std::is_sorted(s.begin(),s.end()));
+}
+
+TEST_CASE("describe_swap", "[aufgabe3.8]")
+{
+	int a = 3;	int b = 5;
+	swap(a,b);
+	REQUIRE(b == 3);
+}
+
 
 int main(int argc, char *argv[])
 {
